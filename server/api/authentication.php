6bf3f4d87665148d97127741cb7dd6c7995a557a
lalbi <lalbizures@gmail.com>
2015-07-04 22:19:33 -0600

    Se agrego servicio para ruta de /session

--- server/api/authentication.php
+++ server/api/authentication.php
@@ -12,6 +12,48 @@ $app->get('/login',function() use ($app){
     echoResponse(200, "termino");
 });
 
+//
+// Segun yo, solo es necesario que mandes el username, ya no valido clave, puesto que se hizo cuando se hizo el login
+$app->post('/session',function() use ($app){
+	// Recupera los datos de la forma
+    $r = json_decode($app->request->getBody());
+    verifyRequiredParams(array('username'),$r->customer);
+
+    $user = $r->customer->username;
+
+    $response = array();
+	//
+	// Verifica si los datos existen en la base de datos.
+	//
+    $db = new DbHandler();
+    $usuario = $db->get1Record("call sp_sel_seg_usuario( '$user' )");
+
+	$opciones = array();
+
+	$response['status'] = "success";
+	$response['message'] = 'Ha ingresado al sistema.';
+	$response['name'] = $usuario['nombre'];
+	$response['uid'] = $usuario['id'];
+	$response['email'] = $usuario['email'];
+	$response['nombres'] = $usuario['nombres'];
+	$response['apellidos'] = $usuario['apellidos'];
+	$response['idrol'] = $usuario['idrol'];
+	$idrol = $usuario['idrol'];
+	$response['rol'] = $usuario['rol'];
+	$response['idorganizacion'] = $usuario['idorganizacion'];
+	$response['organizacion'] = $usuario['organizacion'];
+	$response['idestado'] = $usuario['idestado'];
+	$response['estado'] = $usuario['estado'];
+	
+	$response['fecha'] = $usuario['fecha'];
+
+	// Ya tiene los datos , ahora busca las opciones asignadas de acuerdo a su rol
+	$res = $db->getAllRecord("call sp_sel_rol_opcion( '$idrol' )" );
+	$response['opciones'] = $res;
+			
+    echoResponse(200, $response);
+});
+	
 $app->post('/login',function() use ($app){
 
 	// Recupera los datos de la forma

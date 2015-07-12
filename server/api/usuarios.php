751101732b4a7c1fe5bbad14ddff485c43c16b6a
lalbi <lalbizures@gmail.com>
2015-07-11 22:11:50 -0600

    Rutinas para manejo de usuarios

--- server/api/usuarios.php
+++ server/api/usuarios.php
@@ -11,47 +11,18 @@
    fn_estado()
    pg_estado
    seg_usuario
-   sp_sel_opcion()
-   sp_sel_seg_opcion( )
-   sp_sel_tipo_opcion()
-   sp_sel_seg_opcion_hijos( padre )
    
-   sp_sel_seg_usuario()
- 
+  
+	sp_sel_seg_usuarios()    --- ojo: este debiera ser en singular pero tuve que dejarlo en ploural porque ya existe en singluar para pedir 1 usuario
+                                     en las rutas de authentication.php
+	fn_ins_seg_usuario(
+	sp_sel_seg_usuario_id(?)
+	sp_del_seg_usuario(?)
+	sp_upd_seg_usuario(
+	sp_ins_seg_usuario(
  **/
 
-$app->get('/opLista','sessionAlive', function() use ($app) {
-	$db = new DbHandler();
-    $response = array();
-	$datos = $db->getAllRecord("call sp_sel_tipo_opcion()");
-    if ($datos != NULL) {
-			$response = $datos;
-    }else{
-        $response['status'] = "info";
-        $response['message'] = 'No hay datos';
-    }
-    echoResponse(200, $response);
-});
-
-$app->post('/opListaH','sessionAlive', function() use ($app) {
-    $r = json_decode($app->request->getBody());
-    verifyRequiredParams(array('idPadre'),$r); // 
-    $idPadre = $r->idPadre;
-
-	$db = new DbHandler();
-    $response = array();
-	$datos = $db->getAllRecord("call sp_sel_seg_opcion_hijos($idPadre)");
-    if ($datos != NULL) {
-			$response = $datos;
-    }else{
-        $response['status'] = "info";
-        $response['message'] = 'No hay datos';
-    }
-    echoResponse(200, $response);
-});
- 
-// Opcion para obtener la totalidad de registros de la tabla opcion 
-
+// Opcion para obtener la totalidad de registros de la tabla usuarios
 $app->get('/userDatos','sessionAlive', function() use ($app){
 
     $response = array();
@@ -59,7 +30,7 @@ $app->get('/userDatos','sessionAlive', function() use ($app){
 	// Solicita de la base de datos, la totalidad de registros
 	//
     $db = new DbHandler();
-    $datos = $db->getAllRecord("call sp_sel_seg_usuario( )");
+    $datos = $db->getAllRecord("call sp_sel_seg_usuarios( )");
     if ($datos != NULL) {
 			$response = $datos;
     }else{
@@ -70,21 +41,50 @@ $app->get('/userDatos','sessionAlive', function() use ($app){
     echoResponse(200, $response);
 });
 
-//   Opción para ingresar un registro de la tabla opcion
+// Opcion para obtener un registro de la tabla usuarios
+$app->get('/userR/:id','sessionAlive', function($id) use ($app){
 
-$app->post('/opDatos','sessionAlive',function() use ($app){
+    $response = array();
+	//
+	// Solicita de la base de datos, la totalidad de registros
+	//
+    $db = new DbHandler();
+    $datos = $db->get1Record("call sp_sel_seg_usuarioId(".$id." )");
+    if ($datos != NULL) {
+		$response['status'] = "success";
+		$response['name'] = $datos['nombre'];
+		$response['nombres'] = $datos['nombres'];
+		$response['apellidos'] = $datos['apellidos'];
+		$response['idrol'] = $datos['idrol'];
+		$response['idorganizacion'] = $datos['idorganizacion'];
+		$response['estado'] = $datos['estado'];
+		$response['email'] = $datos['email'];
+    }else{
+        $response['status'] = "info";
+        $response['message'] = 'No hay datos';
+    }
+
+    echoResponse(200, $response);
+});
+
+//   Opción para ingresar un registro de la tabla usuario
+$app->post('/userIn','sessionAlive',function() use ($app){
 
 	// Recupera los datos de la forma
 	//
     $r = json_decode($app->request->getBody());
-    verifyRequiredParams(array('nombre', 'descripcion', 'titulo', 'idPadre','idTipo','orden'),$r->opcion); // 
-    $nombre = $r->opcion->nombre;
-    $descripcion = $r->opcion->descripcion;
-	$titulo = $r->opcion->titulo;
-	$idPadre = $r->opcion->idPadre;
-	$idTipo = $r->opcion->idTipo;
-	$orden = $r->opcion->orden;
-
+	//SELECT `id`, `nombre`, `nombres`, `apellidos`, `clave`, `idrol`, `idorganizacion`, `estado`, `email`, `fecha` FROM `seg_usuario` WHERE 1
+	// la fecha la vamos a guardar desde el store procedure
+    verifyRequiredParams(array('nombre', 'nombres', 'apellidos', 'clave','idrol','idorganizacion','estado','email'),$r->user); // 
+    $nombre = $r->user->nombre;
+    $nombres = $r->user->nombres;
+	$apellidos = $r->user->apellidos;
+	$clave = passwordHash::hash($r->user->clave);
+	$idrol = $r->user->idrol;
+	$idorganizacion = $r->user->idorganizacion;
+	$estado = $r->user->estado;
+	$email = $r->user->email;
+	
     $response = array();
 	//
 	//
@@ -95,34 +95,36 @@ $app->post('/opDatos','sessionAlive',function() use ($app){
     $db = new DbHandler();
 	// $column_names = array('nombre', 'descripcion', 'titulo', 'idPadre','idTipo','orden');
 	// $db->insertIntoTable($r->opcion, $column_names, 'seg_usuario' );
-	$id = $db->get1Record("select fn_ins_seg_opcion( '$nombre','$descripcion','$titulo', $idPadre, $idTipo , $orden ) as id");
+	$id = $db->get1Record("select fn_ins_seg_usuario( '$nombre','$nombres','$apellidos', '$clave', $idrol , $idorganizacion, $estado, '$email' ) as id");
 
     if ($id != NULL) {
         $response['status'] = "success";
         $response['message'] = 'Se agrego correctamente';
-			$response = $id;
+		$response['id'] = $id;
 			
     }else{
         $response['status'] = "info";
-        $response['message'] = 'No hay datos';
+        $response['message'] = 'No fue posible agregar los datos';
     }
-	
 	
     echoResponse(200, $response);
 });
 
-$app->post('/opDatosU','sessionAlive',function() use ($app){
+// 
+$app->post('/userU','sessionAlive',function() use ($app){
 
 	// Recupera los datos de la forma
 	//
     $r = json_decode($app->request->getBody());
-    verifyRequiredParams(array('id','nombre', 'descripcion', 'titulo', 'idPadre','idTipo','orden'),$r->opcion); // 
-    $nombre = $r->opcion->nombre;
-    $descripcion = $r->opcion->descripcion;
-	$titulo = $r->opcion->titulo;
-	$idPadre = $r->opcion->idPadre;
-	$idTipo = $r->opcion->idTipo;
-	$orden = $r->opcion->orden;
+    verifyRequiredParams(array('nombre', 'nombres', 'apellidos', 'clave','idrol','idorganizacion','estado','email'),$r->user); // 
+    $nombre = $r->user->nombre;
+    $nombres = $r->user->nombres;
+	$apellidos = $r->user->apellidos;
+	$clave = passwordHash::hash($r->user->clave);
+	$idrol = $r->user->idrol;
+	$idorganizacion = $r->user->idorganizacion;
+	$estado = $r->user->estado;
+	$email = $r->user->email;
 
     $response = array();
 	//
@@ -132,9 +134,9 @@ $app->post('/opDatosU','sessionAlive',function() use ($app){
 	// select fn_ins_seg_opcion('Ingresa opciones', 'ingreso de opciones', 'Opciones' , 0, 1, 1)
 	//
     $db = new DbHandler();
-    $column_names = array('id','nombre', 'descripcion', 'titulo', 'idPadre','idTipo','orden');
+    $column_names = array('nombre', 'nombres', 'apellidos', 'clave','idrol','idorganizacion','estado','email');
 	// $db->insertIntoTable($r->opcion, $column_names, 'seg_usuario' );
-	$resId = $db->updateRecord("call sp_upd_seg_opcion(?,?,?,?,?,?,?)", $r->opcion, $column_names,'isss');
+	$resId = $db->updateRecord("call sp_upd_seg_usuario(?,?,?,?,?,?,?,?)", $r->user, $column_names,'ssssiiis');
 	
     if ($resId == 0) {
 		$response['status'] = "info";
@@ -149,7 +151,7 @@ $app->post('/opDatosU','sessionAlive',function() use ($app){
     echoResponse(200, $response);
 });
 
-$app->get('/userDatosD/:id','sessionAlive',function($id) use ($app){
+$app->get('/userD/:id','sessionAlive',function($id) use ($app){
 
     $response = array();
 	//

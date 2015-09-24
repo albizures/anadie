515a8c5c17884bd732df1d041aff215c3fbff666
lalbi <lalbizures@gmail.com>
2015-09-24 15:22:00 -0600

    cambios en controller de inscripcion

--- client/app/inscripcion/inscripcion.controller.js
+++ client/app/inscripcion/inscripcion.controller.js
@@ -57,9 +57,12 @@ angular.module('anApp').controller('InscripcionCtrl',
                 templateUrl : 'modalProyectos',
                 controller : 'ModalProyectosCtrl',
                 size : 'lg',
-                proyecto: function () {
-                    return undefined;
-                }
+
+				resolve: {
+					proyecto: function () {
+						return undefined;
+					}
+				}
             });
             modalProyectos.result.then(function (proyecto) {
                 Data.post('proyectoIn',{'proyecto':proyecto})

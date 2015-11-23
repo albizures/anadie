d52f2ff03461a303d20430008ae89fc071e937ab
lalbi <lalbizures@gmail.com>
2015-11-23 08:18:47 -0600

    impresion a pdf

--- server/api/preguntas.php
+++ server/api/preguntas.php
@@ -34,6 +34,151 @@
 		piddocdet ,  pidClave
 		
  **/
+require('lib_pdf/fpdf.php');
+class PDF extends FPDF
+{
+var $widths;
+var $aligns;
+
+function SetWidths($w)
+{
+	//Set the array of column widths
+	$this->widths=$w;
+}
+
+function SetAligns($a)
+{
+	//Set the array of column alignments
+	$this->aligns=$a;
+}
+
+function Row($data)
+{
+	//Calculate the height of the row
+	$nb=0;
+	for($i=0;$i<count($data);$i++)
+		$nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+	$h=5*$nb;
+	//Issue a page break first if needed
+	$this->CheckPageBreak($h);
+	//Draw the cells of the row
+	for($i=0;$i<count($data);$i++)
+	{
+		$w=$this->widths[$i];
+		$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+		//Save the current position
+		$x=$this->GetX();
+		$y=$this->GetY();
+		//Draw the border
+		
+		$this->Rect($x,$y,$w,$h);
+
+		$this->MultiCell($w,5,$data[$i],0,$a,'true');
+		//Put the position to the right of the cell
+		$this->SetXY($x+$w,$y);
+	}
+	//Go to the next line
+	$this->Ln($h);
+}
+
+function CheckPageBreak($h)
+{
+	//If the height h would cause an overflow, add a new page immediately
+	if($this->GetY()+$h>$this->PageBreakTrigger)
+		$this->AddPage($this->CurOrientation);
+}
+
+function NbLines($w,$txt)
+{
+	//Computes the number of lines a MultiCell of width w will take
+	$cw=&$this->CurrentFont['cw'];
+	if($w==0)
+		$w=$this->w-$this->rMargin-$this->x;
+	$wmax=($w-2*$this->cMargin)*1000/$this->FontSize;
+	$s=str_replace("\r",'',$txt);
+	$nb=strlen($s);
+	if($nb>0 and $s[$nb-1]=="\n")
+		$nb--;
+	$sep=-1;
+	$i=0;
+	$j=0;
+	$l=0;
+	$nl=1;
+	while($i<$nb)
+	{
+		$c=$s[$i];
+		if($c=="\n")
+		{
+			$i++;
+			$sep=-1;
+			$j=$i;
+			$l=0;
+			$nl++;
+			continue;
+		}
+		if($c==' ')
+			$sep=$i;
+		$l+=$cw[$c];
+		if($l>$wmax)
+		{
+			if($sep==-1)
+			{
+				if($i==$j)
+					$i++;
+			}
+			else
+				$i=$sep+1;
+			$sep=-1;
+			$j=$i;
+			$l=0;
+			$nl++;
+		}
+		else
+			$i++;
+	}
+	return $nl;
+}
+
+function Header()
+{
+    global $connect, $num_rows;
+
+	if ($_SESSION['uid'] !="")
+	{
+		$id_usuario  = $_SESSION['uid'];
+	}
+	else
+	{
+//		$sql ="select id_usuario from AG1_usuario where usuario = '" . $_SESSION['v_usuario'] . "'" ;
+//		$row = select_datos($sql);
+//		$id_usuario = $row['id_usuario'];
+        $id_usuario = "usario_temp";
+	}
+
+//	$sql = "select mes_operacion, ano_operacion from AG1_config where id_usuario = " . $id_usuario;
+//    $row = select_datos($sql);
+//    $mes_operacion = $row['mes_operacion'];
+//    $ano_operacion = $row['ano_operacion'];	
+
+
+	$this->SetFont('Arial','',9);
+    $this->Cell(200,6,'INFORME DE PREGUNTAS Y RESPUESTAS (SIPREL) - ' . getdate(),1,1);
+	$this->Cell(0,6,' ',0,1);
+	$this->Cell(200,6,'ANADIE',1,1); 
+	$this->Ln(5);
+	$this->Cell(0,6,'LOTE NO.: xxxxx' ,0,0,'C'); 
+	$this->Ln(10);
+}
+
+function Footer()
+{
+	$this->SetY(-15);
+	$this->SetFont('Arial','B',8);
+//	$this->Cell(100,10,'Historial medico',0,0,'L');
+
+}
+
+}
 
 // -- Inserta una pregunta, la primera de un objeto específico. 
 $app->post('/preguntaPrimeraIn','sessionAlive',function() use ($app){
@@ -378,6 +523,22 @@ $app->get('/canSecretarios/:idEvento/:idAmbito','sessionAlive',function($idEvent
 // Imprime - retorna un pdf con todas las preguntas respondidas que se le soliciten en la lista indicada
 $app->post('/printPreguntas','sessionAlive',function() use ($app) {
 
+// Reporte
+
+	$pdf=new PDF('L','mm','Letter');
+	$pdf->Open();
+	$pdf->AddPage();
+	$pdf->SetMargins(5,3,5);  // margen, left, top y right
+	//$pdf->Ln(03);
+
+	//$pdf->SetWidths(array(65, 60, 55, 50, 20));
+	$pdf->SetFont('Arial','B',8);
+	$pdf->SetFillColor(85,107,47);
+    $pdf->SetTextColor(0);
+	
+	$pdf->ln();
+//
+
     $response = array();
 
     $lisPreguntas = json_decode($app->request->getBody());
@@ -405,6 +566,7 @@ $app->post('/printPreguntas','sessionAlive',function() use ($app) {
 					if ($idDet == NULL ) { $ok = false; }
 					else {                                             
 						// Imprime pregunta por pregunta
+						$pdf->Cell(20,4,'Pregunta ' . $idDet,1,0,'L');
 					}
 				}
 			 
@@ -429,11 +591,13 @@ $app->post('/printPreguntas','sessionAlive',function() use ($app) {
         readfile($file);
         exit;*/
         $response['url'] =  "/server/uploaded_files/prueba_impresion.pdf";
+		ob_end_clean();																
+		$pdf->Output();
     }else{
         $response['status'] = "error";
         $response['message'] = 'No se encontro el archivo';
     }
-    echoResponse(200, $response);
+//    echoResponse(200, $response);
 });
 
 ?>
